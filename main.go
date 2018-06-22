fedd57824c8c9ef716e6e3e8791982f08e7a5cc0
konojunya <junya03dance@gmail.com>
2018-06-22 14:12:41 +0900

    ginできた

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("views/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.Run(":7000")
+}
